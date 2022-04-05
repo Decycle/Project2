1e3c920c1fd44143e30b196d892aec9ac59b619f
LeoH10462 <72680151+LeoH10462@users.noreply.github.com>
2022-04-05 10:30:59 -0700

    init stadium

--- stadium.cpp
+++ stadium.cpp
@@ -1,6 +1,53 @@
 #include "stadium.h"
 
-stadium::stadium()
-{
+stadium::stadium(){}
 
+stadium::stadium(string stadium_name, string team_name, string phone, string date, string cap):
+                 stadium_name(stadium_name), team_name(team_name),phone(phone){
+    date_open = "Opened - " +date;
+    capacity = "Capacity - " + cap;
+}
+
+void stadium::info(){
+    cout << getStadiumName() << endl;
+    cout << getTeamName() << endl;
+    cout << getAddress() << endl;
+    cout << getPhone() << endl;
+    cout << getDateOpen() << endl;
+    cout << getCapacity() << endl;
+}
+void stadium::set_address(string street, string zip){
+    address = street + "\n"+zip;
+}
+
+void stadium::set_capacity(string cap){
+    capacity = "Capacity - " + cap;
+}
+
+
+
+const string &stadium::getStadiumName() const {
+    return stadium_name;
+}
+
+const string &stadium::getTeamName() const {
+    return team_name;
+}
+
+const string &stadium::getAddress() const {
+    return address;
+}
+
+const string &stadium::getCapacity() const {
+    return capacity;
+}
+
+const string &stadium::getPhone() const {
+    return phone;
+}
+
+const string &stadium::getDateOpen() const {
+//    string temp = "Opened - " +date_open;
+//    return temp;
+    return date_open;
 }
--- stadium.h
+++ stadium.h
@@ -1,11 +1,37 @@
 #ifndef STADIUM_H
 #define STADIUM_H
+#include <iostream>
+#include <string>
+using namespace std;
 
+using namespace std;
 
-class stadium
-{
+class stadium {
 public:
-    stadium();
-};
 
+    stadium();
+    stadium(string stadium_name, string team, string phone, string date,string cap);
+
+    void set_address(string street, string zip);
+    void set_capacity(string cap);
+    void info();
+private:
+    string stadium_name;
+    string team_name;
+    string address;
+    string phone;
+
+    string capacity;
+    string date_open;
+
+public:
+    const string &getStadiumName() const;
+    const string &getTeamName() const;
+    const string &getAddress() const;
+    const string &getCapacity() const;
+    const string &getPhone() const;
+    const string &getDateOpen() const;
+
+
+};
 #endif // STADIUM_H
